9b8be11239f46bfd750f7e33a995567759253f8f
Nicolas Chan <nicolaschan@berkeley.edu>
2017-11-07 09:45:10 -0800

    Added zip bash script

---
+++ analysis-workflows/notebooks/FindSumerianZipOCR.sh
@@ -0,0 +1,23 @@
+RANGE_DIRECTORY=$1
+DESTINATION="/global/scratch/groups/dh/aanderson/ocr_pages"
+
+if [[ $1 == "" ]]; then
+    echo "Usage: ./FindSumerianZipOCR.sh /path/to/folder/with/hocr"
+    exit 1
+fi
+
+cd $RANGE_DIRECTORY
+
+PDFS=$(ls *.pdf)
+echo $PDFS
+
+module load zip
+
+for PDF in $PDFS; do
+    PDF=$(echo $PDF | cut -d . -f 1)
+    mkdir $PDF
+    cp ${PDF}-*.hocr $PDF/.
+    zip -r ${PDF}.zip $PDF
+done
+
+mv *.zip $DESTINATION
