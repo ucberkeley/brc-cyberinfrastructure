2b8d204d1c66306fe83ab6ee493ab4dc2e127453
Daniel Yan Liu <danielyliu@ln003.brc>
2017-11-07 11:18:46 -0800

    Bash script works with ipynb now

--- analysis-workflows/notebooks/FindSumerianZipOCR.sh
+++ analysis-workflows/notebooks/FindSumerianZipOCR.sh
@@ -1,6 +1,14 @@
 RANGE_DIRECTORY=$1
 DESTINATION="/global/scratch/groups/dh/aanderson/ocr_pages"
 
+#Define module function and MODULEPATH since ipynb doesn't have them in its execution environment
+export MODULEPATH=/global/home/groups/consultsw/sl-7.x86_64/modfiles:/global/home/groups/consultsw/sl-6.x86_64/modfiles:/global/software/sl-6.x86_64/modfiles/tools:/global/software/sl-6.x86_64/modfiles/langs:/clusterfs/vector/home/groups/software/sl-6.x86_64/modfiles:/global/home/groups/vector/software/sl-6.x86_64/modfiles/langs
+
+module ()
+{
+    eval `/usr/Modules/bin/modulecmd bash $*`
+}
+
 if [[ $1 == "" ]]; then
     echo "Usage: ./FindSumerianZipOCR.sh /path/to/folder/with/hocr"
     exit 1
