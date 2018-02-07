0238f09cc5063cd78ccf684c95a96440c615b9b5
Nicolas Chan <nicolaschan@berkeley.edu>
2018-02-07 12:40:49 -0800

    Update gdrive helper to expose nicer API

--- analysis-workflows/notebooks/gdrive.py
+++ analysis-workflows/notebooks/gdrive.py
@@ -42,61 +42,66 @@ def get_service(client_secret):
 def match_extension(extension):
     return lambda file: file['name'][-(len(extension) + 1):] == '.' + extension
 
-def list_files(service, folder_id, extension=None):
-    response = service.files().list(
-        q="'" + folder_id + "' in parents and trashed=false", 
-        spaces='drive',
-        fields='nextPageToken, files(id, name)',
-        pageToken=None).execute()
-    files = response['files']
-    if extension:
-        files = list(filter(match_extension(extension), files))
-    return files
+class GDrive:
+    def __init__(self, client_secret):
+        self.service = get_service(client_secret)
 
-# Source: https://developers.google.com/drive/v3/web/manage-downloads
-# Source: AdamAndersonFindSumerian.ipynb
-def download_file(service, file_id, destination=None):
-    request = service.files().get_media(fileId=google_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while not done:
-        try:
-            status, done = downloader.next_chunk()
-            sys.stdout.write('.')
-        except errors.HttpError as error :
-            print('Error file:', value, '   id:', key)
-            print('An error occurred pulling the next chunk:', error)
-            break
-    fh.seek(0)
-    contents = fh.getvalue()
-    if destination:
-        with open(destination, 'wb') as f2:
-            f2.write(contents)
-            f2.close()
-    fh.close()
-    return contents
+    def list_files(self, folder_id, extension=None):
+        response = self.service.files().list(
+            q="'" + folder_id + "' in parents and trashed=false", 
+            spaces='drive',
+            fields='nextPageToken, files(id, name)',
+            pageToken=None).execute()
+        files = response['files']
+        if extension:
+            files = list(filter(match_extension(extension), files))
+        return files
 
-# Google Drive upload based on AdamAndersonFindSumerian.ipynb
-def upload_file(service, name, path, destination_folder=None):
-    file_metadata = { 'name': name }
-    if destination_folder:
-        file_metadata['parents'] = [destination_folder]
-    media = MediaFileUpload(path, mimetype='text/plain')
-    file = service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields='id'
-    ).execute()
+    # Source: https://developers.google.com/drive/v3/web/manage-downloads
+    # Source: AdamAndersonFindSumerian.ipynb
+    def download_file(self, file_id, destination=None):
+        request = self.service.files().get_media(fileId=google_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while not done:
+            try:
+                status, done = downloader.next_chunk()
+                sys.stdout.write('.')
+            except errors.HttpError as error :
+                print('Error file:', value, '   id:', key)
+                print('An error occurred pulling the next chunk:', error)
+                break
+        fh.seek(0)
+        contents = fh.getvalue()
+        if destination:
+            with open(destination, 'wb') as f2:
+                f2.write(contents)
+                f2.close()
+        fh.close()
+        return contents
 
-def move_file(service, file_id, new_folder, old_folder=None):
-    return service.files().update(fileId=file_id,
-                                  addParents=new_folder,
-                                  removeParents=old_folder).execute()
+    # Google Drive upload based on AdamAndersonFindSumerian.ipynb
+    def upload_file(self, name, path, destination_folder=None):
+        file_metadata = { 'name': name }
+        if destination_folder:
+            file_metadata['parents'] = [destination_folder]
+        media = MediaFileUpload(path, mimetype='text/plain')
+        file = self.service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id'
+        ).execute()
 
-def read_local_file(file_path, encoding='latin-1'):
-    contents = ''
-    with open(file_path, encoding=encoding) as f:
-        for line in f.readlines():
-            contents += line
-    return contents
+    def move_file(self, file_id, new_folder, old_folder=None):
+        return self.service.files().update(
+            fileId=file_id,
+            addParents=new_folder,
+            removeParents=old_folder).execute()
+
+    def read_local_file(self, file_path, encoding='latin-1'):
+        contents = ''
+        with open(file_path, encoding=encoding) as f:
+            for line in f.readlines():
+                contents += line
+        return contents
